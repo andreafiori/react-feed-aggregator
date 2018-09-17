8613896a9ee4a899cadc37e06241bd2c8e714b1a
Andrea Fiori <a.fiori@reply.it>
2018-09-17 19:03:00 +0200

    Dzone test

---
+++ src/feeds/DzoneFeeds.test.js
@@ -0,0 +1,10 @@
+import {  } from './DzoneFeeds';
+
+const assert = require('assert');
+
+describe('Dzone feeds test', function() {
+  // TODO check dzone object values: no doubles path or urls, check required keys
+  /* it('', () => {
+    assert( result === expected );
+  }); */
+});
